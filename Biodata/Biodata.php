148d65c51bb1f8d21b0bde4bed319d693f1d1a58
Mahendra Prayuda Rahman <121299971+mahendrapr99@users.noreply.github.com>
2023-03-07 15:14:58 +0700

    Add files via upload

---
+++ Biodata/Biodata.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="BiodataNew.css">
+<title>About My Self</title>
+</head>
+<audio controls autoplay loop="true" class="hidden">
+    <source src="MARS.mp3" type="audio/mpeg">
+</audio>
+<body style="background-image: url(BACKGROUND_11zon.jpg);background-size:cover;background-attachment: fixed;">
+<div class="wrap">
+    <h1>BIODATA DIRI</h1>
+    <hr style="border-top: 5px double #8c8b8b;">
+    <table>
+    <tr>
+        <td rowspan="10" width="100px">
+        <img src="FOTO_11zon.jpg" class="foto">
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <b>Nama</b>
+            </td>
+            <td>:</td>
+            <td>Mahendra Prayuda Rahman</td>
+    </tr>
+    <tr>
+        <td>
+            <b>NPM</b>
+        </td>
+        <td>:</td>
+        <td>21081010136</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Alamat</b>
+        </td>
+        <td>:</td>
+        <td>Surabaya</td>
+    </tr>
+    <tr>
+        <td>
+            <b>TTL</b>
+        </td>
+        <td>:</td>
+        <td>Jombang, 06 April 2003</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Instagram</b>
+        </td>
+        <td>:</td>
+        <td>@mahendrapryd_</td>
+    </tr>
+    <tr>
+        <td><b>Hobi</b>
+        </td>
+        <td>:</td>
+        <td>Bermain Game, Belajar Ngoding, Videografi</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Cita-cita</b>
+        </td>
+        <td>:</td>
+        <td>Programer, Profesional Pilot Drone</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Github</b>
+        </td>
+        <td>:</td>
+        <td>https://github.com/mahendrapr99</td>
+    </tr>
+</table>
+
+<table>
+<h3>Riwayat Pendidikan</h3>
+    <tr>
+        <td>
+            <b>Sekolah Dasar</b>
+        </td>
+        <td>:</td>
+        <td>MIN 1 Jombang</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Sekolah Menengah Pertama</b>
+        </td>
+        <td>:</td>
+        <td>MTsN 3 Jombang</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Sekolah Menangah Atas</b>
+        </td>
+            <td>:</td>
+            <td>SMAN 1 Jombang</td>
+    </tr>
+    <tr>
+        <td>
+            <b>Perguruan Tinggi</b>
+        </td>
+            <td>:</td>
+            <td>UPN "Veteran" Jawa Timur</td>
+    </tr>
+    </table>
+</div>
+</body>
+</html>
